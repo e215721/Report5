a41d1ce7e73140962aa67bc9d6e955a94d1311d5
e215721 <93558688+e215721@users.noreply.github.com>
2021-12-20 16:09:31 +0900

    Lever3

---
+++ add app/src/main/java/jp/ac/uryukyu/ie/e215721/Main3.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e215721;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException {
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+    }
+}
